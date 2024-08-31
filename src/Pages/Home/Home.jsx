6c4ac6790f2230f3b46ed7e57ff686215c6b3e7c
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-08-31 13:58:44 -0300

    card component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Card } from "../../Components/Card/Card";
 import { Layout } from "../../Components/Layout/Layout";
 
 export const Home = () => {
   return (
   <Layout>
     Home
+    <Card/>
   </Layout>
   );
 };
