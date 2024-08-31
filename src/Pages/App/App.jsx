96511521e9272a9d7b2b5ed381937c87da3edd23
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-08-31 13:43:14 -0300

    navbar

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Home } from "../Home/Home";
 import { MyOrder } from "../MyOrder/MyOrder";
 import { MyAccount } from "../MyAccount/MyAccount";
@@ -6,6 +5,7 @@ import { MyOrders } from "../MyOrders/MyOrders";
 import { NotFound } from "../NotFound/NotFound";
 import { SignIn } from "../SignIn/SignIn";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "../../Components/Navbar/Navbar";
 
 function App() {
   return (
@@ -13,11 +13,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/my-account" element={<MyAccount />}></Route>
-        <Route path="/my-order" element={<MyOrders />}></Route>
+        <Route path="/my-order" element={<MyOrder />}></Route>
         <Route path="/my-orders" element={<MyOrders />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>
+      <Navbar/>
     </BrowserRouter>
   );
 }
