af756ef17270ca0c44056b4d3e5d77e67d6e9cde
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-08-31 12:52:50 -0300

    New route definition

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -5,24 +5,19 @@ import { MyAccount } from "../MyAccount/MyAccount";
 import { MyOrders } from "../MyOrders/MyOrders";
 import { NotFound } from "../NotFound/NotFound";
 import { SignIn } from "../SignIn/SignIn";
-import { useRoutes, BrowserRouter } from "react-router-dom";
-
-const AppRoutes = () => {
-  let routes = useRoutes([
-    {path: "/",element: <Home />},
-    {path: "/my-account",element: <MyAccount />},
-    {path: "/my-order",element: <MyOrder />},
-    {path: "/my-orders",element: <MyOrders />},
-    {path: "/sign-in",element: <SignIn />},
-    {path: "/*",element: <NotFound />},
-  ]);
-  return routes;
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/my-account" element={<MyAccount />}></Route>
+        <Route path="/my-order" element={<MyOrders />}></Route>
+        <Route path="/my-orders" element={<MyOrders />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
