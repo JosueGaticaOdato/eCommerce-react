04d761619385bcd926c8f2ebd631cbf2a320e993
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-08-31 14:17:49 -0300

    Consume API Platzi for the cards

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../Components/Card/Card";
 import { Layout } from "../../Components/Layout/Layout";
 
+export const apiUrl = "https://api.escuelajs.co/api/v1";
+
 export const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data) 
+      } catch (error){
+        console.error(`Ocurrio un error: ${error}`)
+      }
+    }
+    fetchData()
+  },[])
+
   return (
-  <Layout>
-    Home
-    <Card/>
-  </Layout>
+    <Layout>
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      {items?.map(item => (
+        <Card key={item.id} data={item} />
+      ))}
+      </div>
+      
+    </Layout>
   );
 };
